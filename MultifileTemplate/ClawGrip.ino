aaacd30628a537eb76d37927dfb916517623e23d
rhit-lovelamj <152200608+rhit-lovelamj@users.noreply.github.com>
2024-01-16 13:39:18 -0500

    Claw Grip code tweaked to not open/close too much

--- MultifileTemplate/ClawGrip.ino
+++ MultifileTemplate/ClawGrip.ino
@@ -1,16 +1,17 @@
 
-int pos = 0;    // variable to store the servo position 
+int pos = 40;    // variable to store the servo position 
  
-void clawGrab() 
-{ 
-  for(pos = 40; pos < 140; pos += 1)  // goes from 0 degrees to 180 degrees 
+void clawGrab() { 
+  for(pos = 40; pos < 140; pos += 3)  // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(40);                       // waits 40ms for the servo to reach the position 
+    delay(10);                       // waits 40ms for the servo to reach the position 
   } 
-  for(pos = 140; pos>=41; pos-=1)     // goes from 180 degrees to 0 degrees 
+}
+void clawRelease(){
+  for(pos = 140; pos>=41; pos-=3)     // goes from 180 degrees to 0 degrees 
   {                                
     myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(40);                       // waits 40ms for the servo to reach the position 
+    delay(10);                       // waits 40ms for the servo to reach the position 
   } 
-} 
+}
